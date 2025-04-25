d4871e11ba45788e72f8a1de0a78d4483799b014
Vitalii Lukyanov <lukyanov@xparvision.com>
2025-04-25 11:42:22 +0200

    add other scripts i use

--- flake.nix
+++ flake.nix
@@ -1,8 +1,32 @@
 {
   description = "A collection of nushell scripts";
 
-  outputs = {...}: {
+  inputs = {
+    nutest = {
+      url = "github:vyadh/nutest";
+      flake = false;
+    };
+    git_nu = {
+      url = "github:fj0r/git.nu";
+      flake = false;
+    };
+    nupm = {
+      url = "github:nushell/nupm";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    nutest,
+    git_nu,
+    nupm,
+    ...
+  }: {
     utils = ./utils;
     nunps = ./nunps;
+
+    nutest = nutest;
+    git_nu = git_nu;
+    nupm = nupm;
   };
 }
