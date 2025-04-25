4baef1a65756b9e492e15f7942d316e0a0494bfc
Vitalii Lukyanov <lukyanov@xparvision.com>
2025-04-25 12:44:06 +0200

    integration with nu_scripts

--- flake.nix
+++ flake.nix
@@ -2,6 +2,13 @@
   description = "A collection of nushell scripts";
 
   inputs = {
+    nix-lib.url = "github:ekala-project/nix-lib";
+
+    nu_scripts = {
+      url = "github:nushell/nu_scripts";
+      flake = false;
+    };
+
     nutest = {
       url = "github:vyadh/nutest";
       flake = false;
@@ -17,16 +24,44 @@
   };
 
   outputs = {
+    nix-lib,
+    nu_scripts,
     nutest,
     git_nu,
     nupm,
     ...
-  }: {
-    utils = ./utils;
-    nunps = ./nunps;
+  }: let
+    lib = nix-lib.lib;
+  in {
+    theme = selected: let
+      file = builtins.toPath "${nu_scripts}/themes/nu-themes/${selected}.nu";
+    in {
+      use = "use ${file}";
+      set = "$env.config.color_config = (${selected})";
+    };
 
-    nutest = nutest;
-    git_nu = git_nu;
-    nupm = nupm;
+    modules = {
+      utils = ./utils;
+      nunps = ./nunps;
+    };
+
+    external = {
+      modules = let
+        mod_dir = builtins.toPath "${nu_scripts}/modules/";
+      in
+        builtins.mapAttrs (name: _ty: {
+          inherit name;
+          value = builtins.toPath "${mod_dir}/${name}";
+        }) (lib.filterAttrs (_name: ty: ty == "directory") (builtins.readDir mod_dir));
+
+      scripts = {
+        nutest = nutest;
+        git_nu = git_nu;
+        nupm = nupm;
+
+        sourced = builtins.toPath "${nu_scripts}/sourced/";
+        hooks = builtins.toPath "${nu_scripts}/nu-hooks/nu-hooks";
+      };
+    };
   };
 }
